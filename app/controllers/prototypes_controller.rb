ddfb2cb9cc9d0493a4b758507e84e8dd1f38e979
Benishimizu <111289043+Benishimizu@users.noreply.github.com>
2023-03-04 22:01:22 +0900

    コントローラーを作成し、indexアクションの定義とビューファイルの作成
     prototypesコントローラーを作成した
     prototypesコントローラーにindexアクションを定義した（アクションの中には何も記述しない
     viewsディレクトリの中の、prototypesコントローラーに対応するディレクトリにindex.html.erbを作成して、上記の記述をした
    配布済みのstyle.cssを指示されたディレクトリに配置した
     配布済みの画像を、指定されたディレクトリに配置した

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,12 @@
+class PrototypesController < ActionController::Base
+
+  def index
+    # @message = Message.new
+    # @room = Room.find(params[:room_id])
+    # # https://master.tech-camp.in/v2/curriculums/4860
+    # @messages = @room.messages.includes(:user)
+    # # https://master.tech-camp.in/v2/curriculums/4863
+  end
+
+
+end
