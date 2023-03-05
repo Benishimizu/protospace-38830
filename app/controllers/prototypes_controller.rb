c132ffa3078e2ca4bd58d0a2f57e8eeb21d306f6
Benishimizu <111289043+Benishimizu@users.noreply.github.com>
2023-03-05 16:20:07 +0900

    ビューを適切なものに置き換え、画像も配置-2

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,12 +1,7 @@
-class PrototypesController < ActionController::Base
+class PrototypesController < ApplicationController
 
   def index
-    # @message = Message.new
-    # @room = Room.find(params[:room_id])
-    # # https://master.tech-camp.in/v2/curriculums/4860
-    # @messages = @room.messages.includes(:user)
-    # # https://master.tech-camp.in/v2/curriculums/4863
-  end
+     end
 
 
 end
