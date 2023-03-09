6e22292d75e24dc1af183ae3997c8c36e0efe709
Benishimizu <111289043+Benishimizu@users.noreply.github.com>
2023-03-09 21:59:38 +0900

    ログインしていない状態では「ログイン」と「新規登録」のボタンが表示
     ヘッダーの「ログイン」「新規登録」と、「ログアウト」「New Proto」を、ログインをしているときとしていないときで表示が変わるように条件分岐した
     ログインしているときは、トップページに「こんにちはユーザー名さん」が表示されるように条件分岐した
     「こんにちはユーザー名さん」の「ユーザー名」の部分に、今ログインしているユーザーのユーザー名が表示されるようにした
    
    --------------
    
    user_signed_in?ユーザー サインド インメソッド
    Gemのdeviseを導入しているため、使用できるメソッドです。
    ログインしているかどうかの判定を行います。
    
    user_signed_in?メソッドは、ユーザーがログインしていればtrueを、ログアウト状態であればfalseを返します。
    
    【例】user_signed_in?
    1
    2
    3
    4
    5
    6
    7
    # ログインしているユーザーのとき
    user_signed_in?
    #=> true
    
    # ログインしていないユーザーのとき
    user_signed_in?
    #=> false
    
    表示について：
    <%= @post %>  <%# RubyコードをHTMLに埋め込む %>
    また、上記の例のように<% %>の中に#を用いると、Rubyファイルの時と同様にコメントアウトすることができます。
    
    上記の例は、HTMLにRubyのコードを埋め込み、画面上に表示させる記述方法でした。
    一方で、画面上に表示をさせなくても良いRuby記法（if文や、each文など）の場合には、=を抜いて<% %>でコードを囲います。
    たとえば以下のような表記にすると、インスタンス変数に定義した文字列は表示されなくなることがわかります。試しにindex.html.erbを修正してみましょう。
    
    <% @post %>   <%#  RubyコードをHTMLに埋め込むが表示はされない %>
    
    link_toで作成したリンクは、デフォルトでGETメソッドを使用します。
    そのため、DELETEメソッドを使用する際はmethodオプションに:deleteの指定が必要です。GETメソッドを使用する場合は、methodオプションを省略できます。
    
    また、ログアウト以外にはclassオプションを使い、class属性をつけることで、CSSが適用されるようにしています。

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,2 +1,8 @@
 class PrototypesController < ApplicationController
+
+
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "prototypes#index"
-
+  resources :prototypes
+  # https://master.tech-camp.in/v2/curriculums/4220
+  # https://master.tech-camp.in/v2/curriculums/4219
 end
