6a925dbe6c4c9efd1fbdaaa386165f42dcf900ff
Benishimizu <111289043+Benishimizu@users.noreply.github.com>
2023-03-27 23:24:42 +0900

    ユーザー詳細ページ
    ユーザー詳細ページでユーザーの情報が表示されるようにしましょう
    遷移先のユーザー詳細ページで、そのユーザーの情報と、そのユーザーが投稿したプロトタイプの情報が過不足無く表示されるようにしましょう。
    
    作業チェック
     usersコントローラーのshowアクションにインスタンス変数@userを定義した。且つ、Pathパラメータで送信されるID値で、Userモデルの特定のオブジェクトを取得するように記述し、それを@userに代入した
     ユーザー詳細ページで、ユーザーの各情報が正しく表示できるように修正した
     ユーザー詳細ページで、そのユーザーが投稿したプロトタイプが表示されるように、_prototype.html.erbの部分テンプレートを呼び出した（collectionオプションを使用する）
     サーバーを再起動して、正しく表示されることを確認した
    参考カリキュラム
    マイページを実装しよう
    メッセージ投稿機能を実装しよう
    
    ーーーーー
    
    https://master.tech-camp.in/v2/curriculums/4223
    https://master.tech-camp.in/v2/curriculums/4762
    
    +
     usersコントローラーにshowアクションを定義した（まだアクション内の処理は書かない）

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,27 +4,5 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
-  def show
-    # @nickname = current_user.nickname
-    # @tweets = current_user.tweets
-  end
-  # ユーザーに関するshowアクションをコントローラーに定義しよう
-      # users_controller.rbを作成し、そこへshowアクションを定義します。
-
-      # コマンドを実行してコントローラーを作成しましょう
-      # users_controller.rbを作成します。
-
-      # ターミナル
-      # # users_controllerと関連するファイルを作成
-      # % rails g controller users
-      # users_controller.rbを編集しましょう
-      # showアクションで表示するのは、ログイン中ユーザーのマイページとなるため、
-      # 必要な情報は「ニックネーム」と「ログイン中のユーザーのツイート投稿」の2つです。
-
-      # それぞれを@nicknameと@tweetsというインスタンス変数に代入します。
-    # 
-
-
-
 
 end
